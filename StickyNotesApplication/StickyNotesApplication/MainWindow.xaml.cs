dfe34d45f48bd5cd6e970647bfa6190c1d513973
Sahan62 <dogansahan62@gmail.com>
2026-02-11 11:46:05 +0100

    Basic Layout of a Sticky Note

---
+++ StickyNotesApplication/StickyNotesApplication/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StickyNotesApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /*private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // This code runs when the button is clicked
+            text1.Text = "Button clicked";
+        }*/
+
+      
+        //Method for clsoing application
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Method for dragging the window
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+    }
+}
